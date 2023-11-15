badd10fabc075e874c920a0ee97627ebdf2b7cb1
Agonpas <agonpas@gmail.com>
2023-11-15 18:08:05 +0100

    Dando atributo nombre

--- Nivel 1/Ejercicio1/Animal.php
+++ Nivel 1/Ejercicio1/Animal.php
@@ -1,5 +1,12 @@
 <?php
 class Animal {
+    protected $nombre;
+    public function __construct($nombre) {
+        $this->nombre = $nombre;
+    }
+    public function getNombre() {
+        return $this->nombre;
+    }
     public function hablar() {
 
     }
--- Nivel 1/Ejercicio1/ejercicio1.php
+++ Nivel 1/Ejercicio1/ejercicio1.php
@@ -3,12 +3,12 @@ include_once 'Gato.php'; //llamamos a la familia gato
 include_once 'Perro.php'; // llamammos a la familia perro
 
 /*instanciamos un objeto de la familia gato y aplicamos el método hablar */
-$felix = new Gato;
+$gato1 = new Gato("Félix");
 echo PHP_EOL;
-echo "El gato hace: " . $felix->hablar() . PHP_EOL;
+echo "El gato " . $gato1->getNombre() . " hace: " . $gato1->hablar() . PHP_EOL;
 
 /*instanciamos un objeto de la familia perro y aplicamos el método hablar */
-$pluto = new Perro;
+$perro1 = new Perro("Pluto");
 echo PHP_EOL;
-echo "El perro hace: " . $pluto->hablar() . PHP_EOL;
+echo "El perro " . $perro1->getNombre() . " hace: " . $perro1->hablar() . PHP_EOL;
 ?>
